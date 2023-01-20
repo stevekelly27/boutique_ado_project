ad0ab406a595828ced5eb730b0886d40fa9ddac7
stevekelly27 <stevekelly28@hotmail.com>
2023-01-20 20:31:26 +0000

    added quantity boxes to shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -28,7 +28,7 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
@@ -39,9 +39,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
